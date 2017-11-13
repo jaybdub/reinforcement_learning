9c00de0aa64bf5f0164f54f98ca709ad346a25e7
John Welsh <jbmy13@gmail.com>
2017-11-13 17:05:43 -0500

    added cart pole model

---
+++ cart_pole/model.py
@@ -0,0 +1,21 @@
+# Neural network model(s) for the pygym 'CartPoleEnv'
+#
+# author: John Welsh
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class CartPoleModel(nn.Module):
+
+    def __init__(self):
+        self.fc1 = nn.Linear(4, 32)
+        self.fc2 = nn.Linear(32, 16)
+        self.fc3 = nn.Linear(16, 2)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        return x
