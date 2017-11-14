bd01184fe605ef33496ce6a77d1ea51370f40d1a
John Welsh <jbmy13@gmail.com>
2017-11-13 23:40:36 -0500

    runs

--- cart_pole/model.py
+++ cart_pole/model.py
@@ -18,5 +18,5 @@ class CartPoleModel(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
+        x = self.fc3(x)
         return x
--- cart_pole/train.py
+++ cart_pole/train.py
@@ -1,6 +1,7 @@
+import pdb
 import torch
 from transition import Transition
-from torch import Tensor
+from torch import Tensor, LongTensor
 import numpy as np
 from gym.envs.classic_control import CartPoleEnv
 from replay import ReplayMemory
@@ -8,6 +9,8 @@ from model import CartPoleModel
 from torch.optim import SGD, Adam
 from torch.utils.data.dataloader import DataLoader, BatchSampler, \
                                         RandomSampler, SequentialSampler
+import torch.nn.functional as F
+from torch.autograd import Variable
 
 # arguments
 DEFAULT_NUM_EPISODES = 500
@@ -20,6 +23,7 @@ DEFAULT_SAMPLER = 'random'
 DEFAULT_OPTIMIZER = 'sgd'
 DEFAULT_OPTIMIZER_LR = 0.01
 DEFAULT_OPTIMIZER_MOMENTUM = 0.0
+GAMMA = 0.99
 
 optimizers = {
     'sgd': SGD
@@ -32,6 +36,7 @@ samplers = {
 
 def tensor_argmax(t):
     return t.data.numpy().argmax()
+
 
 if __name__ == '__main__':
 
@@ -56,22 +61,45 @@ if __name__ == '__main__':
 
     env = CartPoleEnv()
 
-    for episode in range(DEFAULT_OPTIMIZER_LR):
+    for episode in range(DEFAULT_NUM_EPISODES):
 
         state = env.reset() 
         done = False
         step = 0
 
         while step < DEFAULT_MAX_EPISODE_LENGTH and not done:
+            # env.render()
+
             # sample action
-            action = tensor_argmax(model(Tensor(state)))
+            action = tensor_argmax(model(Variable(Tensor(state))))
 
             # step environment
             next_state, reward, done, info = env.step(action)
 
             # add to replay memory
-            replay.push(Transition((state, action, reward, next_state)))
+            replay.push(Transition(state, action, reward, next_state))
+            state = next_state
 
             # (update model?)
             step += 1
+
+
+        # update model
+        pdb.set_trace()
+        batch = next(sampler.__iter__())
+
+        q_now = model(Variable(batch[0].float()))
+        q_next = model(Variable(batch[3].float(), volatile=True))
+
+        reward_now = q_now.gather(1, Variable(batch[1][:, None]))
+        reward_next = q_next.max(1)[0][:, None]
+        reward_next.volatile = False
+        reward_now_target = reward_now + GAMMA * reward_next 
+
+        loss = F.soft_margin_loss(reward_now, reward_now_target)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
 
